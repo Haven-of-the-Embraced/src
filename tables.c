57258893d604be2b5d1c42109e9de038bdb9f2e8
Matthew Sexton <mssxtn@gmail.com>
2014-07-17 19:48:51 -0400

    Changed opensesame to open pickproof locks, upped cost

--- tables.c
+++ tables.c
@@ -1495,7 +1495,7 @@ const struct    rote_type   rote_table  [MAX_ROTE]  =
     {   "Cellular Mitosis",     rote_cellularmitosis,       0, 0, 0, 5, 0, 0, 2, 0, 0, TARGET_CHAR, 25, 7, TRUE},
     {   "Matter Perception",    rote_matterperception,      0, 0, 0, 0, 1, 0, 0, 0, 0, TARGET_NONE, 1, 1, FALSE},
     {   "Golden Wish",          rote_goldenwish,            0, 0, 0, 0, 2, 0, 0, 0, 0, TARGET_NONE, 2, 2, TRUE},
-    {   "Open Sesame",          rote_opensesame,            0, 0, 0, 0, 3, 0, 0, 0, 0, TARGET_NONE, 3, 3, TRUE},
+    {   "Open Sesame",          rote_opensesame,            0, 0, 0, 0, 3, 0, 0, 0, 0, TARGET_NONE, 5, 7, TRUE},
     {   "Quilted Form",         rote_quiltedform,           0, 0, 0, 0, 4, 0, 0, 0, 0, TARGET_OBJ,  4, 4, TRUE},
     {   "Refine Matter",        rote_refinematter,          0, 0, 0, 0, 5, 0, 0, 0, 0, TARGET_OBJ,  25, 7, TRUE},
     {   "Empower Self",         rote_empowerself,           0, 0, 0, 0, 0, 1, 0, 0, 0, TARGET_NONE, 1, 1, FALSE},

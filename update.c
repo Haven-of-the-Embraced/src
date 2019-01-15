153bb09f5cd32c37cb9bd9d4c5c63bce78fd527e
Matthew Sexton <mssxtn@gmail.com>
2019-01-14 23:16:43 -0500

    Moved save_config call in update_handler to hopefully kill lag

--- update.c
+++ update.c
@@ -1748,6 +1748,7 @@ CHAR_DATA   *ch;
     /*number_range( PULSE_AREA / 2, 3 * PULSE_AREA / 2 );*/
     area_update ( );
     quest_update     ( );
+    save_config();
     }
 
     if ( --pulse_music    <= 0 )
@@ -1813,7 +1814,6 @@ CHAR_DATA   *ch;
 /* number_range( PULSE_TICK / 2, 3 * PULSE_TICK / 2 ); */
     char_update ( );
     obj_update  ( );
-    save_config();
     }
 
     sleep_update ();

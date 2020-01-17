3739166b0cf1d918ad787add03a89508e2fd09d1
Matthew Sexton <wsdmatty@gmail.com>
2020-01-17 11:03:14 -0500

    affect_update: Moved shadowplay affect here, adjusted, changed poison
    (#20, #89)
    Poison now rolls stamina, if failed take damage. Damage is max_hit / 
    (50*stamina)
    
    Nightshades has a (obten_level*5) percent chance of firing. If it does, 
    does max_hit / 10(15-obten_level) damage.

--- pulse.c
+++ pulse.c
@@ -13,17 +13,28 @@ void affects_update (void) {
     CHAR_DATA *ch_next;
     AFFECT_DATA af;
 
-
     /* Examine all mobs. */
     for ( ch = char_list; ch != NULL; ch = ch_next )
     {
     ch_next = ch->next;
 
     //EMERGOD POISON ACTUALLY SUCKS NOW CODE
-    if ( IS_AFFECTED (ch, AFF_POISON) && ch->hit > (ch->max_hit / 20) )
-        damage(ch,ch,
-            (ch->hit/(50*get_attribute(ch, STAMINA) > 0 ? get_attribute(ch, STAMINA) : 1 )),
+    if ( IS_AFFECTED (ch, AFF_POISON) && ch->hit > (ch->max_hit / 20) &&
+        godice(get_attribute(ch, STAMINA), 8) < 1)
+        damage(ch, ch, (ch->max_hit / (50*get_attribute(ch, STAMINA))),
             gsn_poison,DAM_POISON,FALSE);
+
+    if (is_affected(ch, gsn_shadowplay))
+    {
+        int level = get_affect_level(ch, gsn_shadowplay);
+        if (number_percent() < 5 * level)
+        {
+            act( "$n chokes and gags as shadows try to suffocate $m.", ch, NULL, NULL, TO_ROOM );
+            send_to_char( "You gag and struggle to breathe through the shadows in your throat.\n\r", ch );
+            if(ch->position != POS_TORPOR)
+                damage(ch,ch,ch->max_hit/(10*(15-level)),gsn_shadowplay,DAM_BASH,FALSE);
+        }
+    }
 
     // Player-Only affects below this point.
     if (IS_NPC(ch))

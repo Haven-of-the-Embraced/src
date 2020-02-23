c6892e9a4552c8cd3baae3fdf39e408e12ee127c
Matthew Sexton <wsdmatty@gmail.com>
2020-02-22 19:48:38 -0500

    do_enter: fixed a missing if check (#107)

--- act_enter.c
+++ act_enter.c
@@ -143,7 +143,7 @@ void do_enter( CHAR_DATA *ch, char *argument)
     char_from_room(ch);
     char_to_room(ch, location);
 
-    if (!pass_gauntlet(ch))
+    if (IS_SET(portal->value[2], GATE_UMBRA) && !pass_gauntlet(ch))
     {
 	    char_from_room(ch);
 	    char_to_room(ch, old_room);

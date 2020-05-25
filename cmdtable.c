d30fc062b1094c2ab3d3bf60c78d301ef8a691c7
Matthew Sexton <wsdmatty@gmail.com>
2020-05-25 07:44:29 -0400

    cmdfun_table: removed erroneous space from name preventing dofun from being used

--- cmdtable.c
+++ cmdtable.c
@@ -183,7 +183,7 @@ const   struct  cmdfun_type cmdfun_table    [] =
     { "horridform",     do_horrid },
     { "essence",        do_essence},
     { "chiropteranmarauder",	do_chiropteranmarauder },
-    { "wing claws", do_wingclaws },
+    { "wingclaws", do_wingclaws },
     { "dash",       do_dash },
     { "ignisfatuus", do_ignis_fatuus},
     { "mortalterrors", do_mortalterrors},

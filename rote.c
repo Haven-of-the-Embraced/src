dbc2f5ec5a48c88c276360b7692217c2a8440a6d
Nicholas <njw352@gmail.com>
2020-06-20 11:26:38 -0400

    destroymatter: removed debugging code
    #132

--- rote.c
+++ rote.c
@@ -603,12 +603,6 @@ void rote_destroymatter( CHAR_DATA *ch, int success, CHAR_DATA *victim )
         return;
     }
 
-    if (IS_DEBUGGING(ch))
-    {
-      sprintf(buf,"{CRote Successes: {M%d{x\n\r", success);
-      send_to_char(buf, ch);
-    }
-
     if ( ( obj = get_eq_char( victim, WEAR_WIELD ) ) == NULL )
       if ( ( obj = get_eq_char( victim, WEAR_SHIELD ) ) == NULL )
         if ( ( obj = get_eq_char( victim, WEAR_HOLD ) ) == NULL )
@@ -617,17 +611,12 @@ void rote_destroymatter( CHAR_DATA *ch, int success, CHAR_DATA *victim )
           return;
         }
 
-        success = -1;
-
     if (success < 0)
     {
       act("The feedback from trying to alter reality causes your equipment to heat and burn!", ch, obj, victim, TO_CHAR);
       act("$n glances in your direction and yelps in pain.", ch, obj, victim, TO_VICT);
       act("$n yelps in pain and surprise!", ch, obj, victim, TO_NOTVICT);
-/* Need to figure out how to trigger heat metal on self.
-  Syntax: spell_heat_metal( int sn, int level, CHAR_DATA *ch, void *vo,int target )*/
       spell_heat_metal( gsn_heat_metal, ch->level / 2 + 50, ch, ch, 0 );
-
       return;
     }
 

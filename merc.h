c35376d6c26696d02c6e2561f08e612cf23b0b9a
Nicholas <njw352@gmail.com>
2020-06-21 16:19:26 -0400

    Increased MAX_SKILL for 'cloak of shadows'

--- merc.h
+++ merc.h
@@ -183,7 +183,7 @@ typedef void ROOM_FUN	args( ( ROOM_INDEX_DATA *room, char *argument ) );
  */
 #define MAX_HELP        5
 #define MAX_SOCIALS       256
-#define MAX_SKILL         410
+#define MAX_SKILL         411
 #define MAX_GIFT          90
 #define MAX_GIFTS_CODED   163
 #define MAX_BREW        24

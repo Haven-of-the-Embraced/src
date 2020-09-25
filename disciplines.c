b998f4979b3d57ea9a472ae03c23f5b8e994fd94
Nicholas <njw352@gmail.com>
2020-09-25 19:05:43 -0400

    do_reveal: fixed to add proper vulnerability
    #418

--- disciplines.c
+++ disciplines.c
@@ -697,6 +697,7 @@ void do_reveal(CHAR_DATA *ch, char *argument)
     af.bitvector = AFF_DETECT_HIDDEN;
     affect_to_char( ch, &af );
 
+    af.where     = TO_VULN;
     af.bitvector = VULN_SOUND;
     affect_to_char( ch, &af );
 

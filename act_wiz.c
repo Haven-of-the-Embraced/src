6a9fb405a1aa9da20e74977ac5d531fb9dfd18ba
Nicholas <njw352@gmail.com>
2020-12-05 16:53:13 -0500

    mstat: added options for resist/vuln/imm bits
    #427

--- act_wiz.c
+++ act_wiz.c
@@ -2327,6 +2327,8 @@ void do_mstat( CHAR_DATA *ch, char *argument )
         affect_loc_name( paf->location ),
         paf->modifier,
         paf->duration,
+        paf->where == TO_RESIST ? imm_bit_name(paf->bitvector) : paf->where == TO_VULN ? imm_bit_name(paf->bitvector) :
+        paf->where == TO_IMMUNE ? imm_bit_name(paf->bitvector) :
         paf->where == TO_AFFECTS2 ? affect2_bit_name(paf->bitvector) : affect_bit_name( paf->bitvector ),
         paf->level
         );

eccdc16fc80244d422cea2731a1926100330e513
Matthew Sexton <wsdmatty@gmail.com>
2021-05-26 20:47:26 -0400

    do_enter: moved to act_move b/c its a movement action.

--- act_move.c
+++ act_move.c
@@ -2252,3 +2252,218 @@ void do_hometown (CHAR_DATA *ch, char *argument)
     }
 
 }
+
+/* random room generation procedure */
+ROOM_INDEX_DATA  *get_random_room(CHAR_DATA *ch)
+{
+    ROOM_INDEX_DATA *room;
+
+    for ( ; ; )
+    {
+        room = get_room_index( number_range( 0, 65535 ) );
+        if ( room != NULL )
+        if ( can_see_room(ch,room)
+	&&   !room_is_private(room)
+        &&  !IS_SET(room->area->area_flags, AREA_NOTELE)
+        &&  !IS_SET(room->area->area_flags, AREA_UNLINKED)
+        &&   !IS_SET(room->room_flags, ROOM_PRIVATE)
+        &&   !IS_SET(room->room_flags, ROOM_UMBRA)
+        &&   !IS_SET(room->room_flags, ROOM_SOLITARY)
+	&&   !IS_SET(room->room_flags, ROOM_SAFE)
+	&&   (IS_NPC(ch) || IS_SET(ch->act,ACT_AGGRESSIVE)
+	||   !IS_SET(room->room_flags,ROOM_LAW)))
+            break;
+    }
+
+    return room;
+}
+
+/* RT Enter portals */
+void do_enter( CHAR_DATA *ch, char *argument)
+{
+    ROOM_INDEX_DATA *location;
+
+    if ( ch->fighting != NULL )
+	return;
+
+    /* nifty portal stuff */
+    if (argument[0] != '\0')
+    {
+        ROOM_INDEX_DATA *old_room;
+	OBJ_DATA *portal;
+	CHAR_DATA *fch, *fch_next;
+
+        old_room = ch->in_room;
+
+	portal = get_obj_list( ch, argument,  ch->in_room->contents );
+
+	if (portal == NULL)
+	{
+	    send_to_char("You don't see that here.\n\r",ch);
+	    return;
+	}
+
+	if (portal->item_type != ITEM_PORTAL
+        ||  (IS_SET(portal->value[1],EX_CLOSED) && !IS_TRUSTED(ch,ANGEL)))
+	{
+	    send_to_char("You can't seem to find a way in.\n\r",ch);
+	    return;
+	}
+
+	if (!IS_TRUSTED(ch,ANGEL) && !IS_SET(portal->value[2],GATE_NOCURSE)
+	&&  (IS_AFFECTED(ch,AFF_CURSE)
+	||   IS_SET(old_room->room_flags,ROOM_NO_RECALL)))
+	{
+	    send_to_char("Something prevents you from leaving...\n\r",ch);
+	    return;
+	}
+
+  if (IS_SET(portal->value[2], GATE_UMBRA) && is_affected(ch, gsn_astralprojection))
+  {
+      act("Your astral form refuses to cross through $p.  You must follow your silver cord to your waiting body.", ch, portal, NULL, TO_CHAR);
+      act("$n's form wavers for a moment as it refuses to cross into $p.", ch, portal, NULL, TO_NOTVICT);
+      return;
+  }
+
+  if (IS_SET(portal->value[2],GATE_RANDOM) || portal->value[3] == -1)
+	{
+	    location = get_random_room(ch);
+	    portal->value[3] = location->vnum; /* for record keeping :) */
+	}
+	else if (IS_SET(portal->value[2],GATE_BUGGY) && (number_percent() < 5))
+	    location = get_random_room(ch);
+	else
+	    location = get_room_index(portal->value[3]);
+
+	if (location == NULL
+	||  (location == old_room && !IS_SET(portal->value[2], GATE_UMBRA))
+	||  !can_see_room(ch,location))
+	{
+	   act("$p doesn't seem to go anywhere.",ch,portal,NULL,TO_CHAR);
+	   return;
+	}
+
+    if (portal->level <= 100 && room_is_private(location) && !IS_TRUSTED(ch,IMPLEMENTOR))
+	{
+	   act("$p doesn't seem to go anywhere.",ch,portal,NULL,TO_CHAR);
+	   return;
+	}
+
+        if (IS_NPC(ch) && IS_SET(ch->act,ACT_AGGRESSIVE)
+        &&  IS_SET(location->room_flags,ROOM_LAW))
+        {
+            send_to_char("Something prevents you from leaving...\n\r",ch);
+            return;
+        }
+
+	act("$n steps into $p.",ch,portal,NULL,TO_ROOM);
+
+	if (IS_SET(portal->value[2],GATE_NORMAL_EXIT))
+	    act("You enter $p.",ch,portal,NULL,TO_CHAR);
+	else
+	    act("You walk through $p and find yourself somewhere else...",
+	        ch,portal,NULL,TO_CHAR);
+
+    char_from_room(ch);
+    char_to_room(ch, location);
+
+    if (IS_SET(portal->value[2], GATE_UMBRA) && !pass_gauntlet(ch, TRUE))
+    {
+	    char_from_room(ch);
+	    char_to_room(ch, old_room);
+        sendch("It is too dangerous to continue onward.\n\r", ch);
+        return;
+    }
+
+	if (IS_SET(portal->value[2],GATE_GOWITH)) /* take the gate along */
+	{
+	    obj_from_room(portal);
+	    obj_to_room(portal,location);
+	}
+
+
+	if (IS_SET(portal->value[2],GATE_NORMAL_EXIT))
+	    act("$n has arrived.",ch,portal,NULL,TO_ROOM);
+	else
+	    act("$n has arrived through $p.",ch,portal,NULL,TO_ROOM);
+
+	do_function(ch, &do_look, "auto");
+
+	/* charges */
+	if (portal->value[0] > 0)
+	{
+	    portal->value[0]--;
+	    if (portal->value[0] == 0)
+		portal->value[0] = -1;
+	}
+
+	/* protect against circular follows */
+	if (old_room == location && !IS_SET(portal->value[2], GATE_UMBRA))
+	    return;
+
+    	for ( fch = old_room->people; fch != NULL; fch = fch_next )
+    	{
+            fch_next = fch->next_in_room;
+
+            if (portal == NULL || portal->value[0] == -1)
+	    /* no following through dead portals */
+                continue;
+
+            if ( fch->master == ch && IS_AFFECTED(fch,AFF_CHARM)
+            &&   fch->position < POS_STANDING)
+            	do_function(fch, &do_stand, "");
+
+            if ( fch->master == ch && fch->position == POS_STANDING)
+            {
+
+                if (IS_SET(ch->in_room->room_flags,ROOM_LAW)
+                &&  (IS_NPC(fch) && IS_SET(fch->act,ACT_AGGRESSIVE)))
+                {
+                    act("You can't bring $N into the city.",
+                    	ch,NULL,fch,TO_CHAR);
+                    act("You aren't allowed in the city.",
+                    	fch,NULL,NULL,TO_CHAR);
+                    continue;
+            	}
+
+            	act( "You follow $N.", fch, NULL, ch, TO_CHAR );
+		do_function(fch, &do_enter, argument);
+            }
+    	}
+
+ 	if (portal != NULL && portal->value[0] == -1)
+	{
+	    act("$p fades out of existence.",ch,portal,NULL,TO_CHAR);
+	    if (ch->in_room == old_room)
+		act("$p fades out of existence.",ch,portal,NULL,TO_ROOM);
+	    else if (old_room->people != NULL)
+	    {
+		act("$p fades out of existence.",
+		    old_room->people,portal,NULL,TO_CHAR);
+		act("$p fades out of existence.",
+		    old_room->people,portal,NULL,TO_ROOM);
+	    }
+	    extract_obj(portal);
+	}
+
+
+	/*
+	 * If someone is following the char, these triggers get activated
+	 * for the followers before the char, but it's safer this way...
+	 */
+	if ( IS_NPC( ch ) && HAS_TRIGGER_MOB( ch, TRIG_ENTRY ) )
+	    p_percent_trigger( ch, NULL, NULL, NULL, NULL, NULL, TRIG_ENTRY );
+	if ( !IS_NPC( ch ) )
+    {
+        p_greet_trigger( ch, PRG_MPROG );
+        p_greet_trigger( ch, PRG_OPROG );
+        p_greet_trigger( ch, PRG_RPROG );
+    }
+
+
+	return;
+    }
+
+    send_to_char("Nope, can't do it.\n\r",ch);
+    return;
+}

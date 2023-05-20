4af7f2693b471b9ae87de5bef3b8044598b7c0f8
Soul <sgtgemini@gmail.com>
2023-05-19 20:12:12 -0500

    Missed duped "COMM_NOWIZ", changed to COMM_NOADMIN

--- act_comm.c
+++ act_comm.c
@@ -153,7 +153,7 @@ void do_channels(CHAR_DATA *ch, char *argument)
 
     if (IS_ADMIN(ch))
     {
-        printf_to_char(ch, "%-18s  %-4s\n\r", "{wImp Channel{x", (!IS_SET(ch->comm, COMM_NOWIZ)) ? "{GON{x" : "{ROFF{x");
+        printf_to_char(ch, "%-18s  %-4s\n\r", "{wImp Channel{x", (!IS_SET(ch->comm, COMM_NOADMIN)) ? "{GON{x" : "{ROFF{x");
     }
 
     printf_to_char(ch, "%-18s  %-4s\n\r", "{wShouts{x", (!IS_SET(ch->comm, COMM_SHOUTSOFF)) ? "{GON{x" : "{ROFF{x");

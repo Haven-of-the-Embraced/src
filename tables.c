ac75e3d875d855151f48df68d0a6ec3b9b19aa83
Nicholas <njw352@gmail.com>
2023-06-20 22:28:18 -0400

    channel: success gives quintessence, adjusted quint cost (closes #984)

--- tables.c
+++ tables.c
@@ -1624,7 +1624,7 @@ const struct    rote_type   rote_table  [MAX_ROTE]  =
     {   "Control Minds",        rote_controlminds,          0, 0, 0, 0, 0, 4, 0, 0, 0, TARGET_CHAR, 4, 4, TRUE, FALSE},
 		{		"Prime Sense",					rote_primesense,						0, 0, 0, 0, 0, 0, 1, 0, 0, TARGET_NONE, 1, 1, FALSE, FALSE},
     {   "Quintessence Bolt",    rote_quintessencebolt,      0, 0, 0, 0, 0, 0, 2, 0, 0, TARGET_CHAR, 2, 2, TRUE, TRUE},
-    {   "Quintessence Bolt",    rote_channelquintessence,   0, 0, 0, 0, 0, 0, 3, 0, 0, TARGET_OBJ,  1, 3, TRUE, TRUE},
+    {   "Channel Quintessence", rote_channelquintessence,   0, 0, 0, 0, 0, 0, 3, 0, 0, TARGET_OBJ,  3, 3, TRUE, TRUE},
     {   "Spirit Sight",         rote_spiritsight,           0, 0, 0, 0, 0, 0, 0, 1, 0, TARGET_NONE, 1, 1, FALSE, FALSE},
     {   "Call Spirit",          rote_callspirit,            0, 0, 0, 0, 0, 0, 0, 2, 0, TARGET_NONE, 10, 2, TRUE, TRUE},
     {   "Step Sideways",        rote_stepsideways,          0, 0, 0, 0, 0, 0, 0, 3, 0, TARGET_NONE, 3, 3, TRUE, TRUE},

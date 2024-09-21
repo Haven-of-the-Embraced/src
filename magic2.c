c4ac8ded417de8a672a892e256a9532b870850ab
Nicholas <njw352@gmail.com>
2024-09-20 21:33:38 -0400

    vampblood: removed ghoul bonus (#136)

--- magic2.c
+++ magic2.c
@@ -418,8 +418,6 @@ void spell_vampire_blood( int sn, int level, CHAR_DATA *ch, void *vo, int target
         act("$n gags and spits out the blood.",ch,NULL,NULL,TO_ROOM);
         return;
 	}
-	if (ch->race == race_lookup("ghoul"))
-		ch->pblood += level;
 
     ch->pblood += level;
     if(ch->pblood > ch->max_pblood)

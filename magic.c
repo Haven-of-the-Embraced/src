ddce04d14a502da8c1205445deb7ab8f06779787
Nicholas <njw352@gmail.com>
2025-05-29 22:43:03 -0400

    lightningbolt: dmg diff 6 (#496)

--- magic.c
+++ magic.c
@@ -3636,7 +3636,7 @@ void spell_lightning_bolt(int sn,int level,CHAR_DATA *ch,void *vo,int target)
     int dam;
 
     if (!IS_NPC(ch))
-        dam = godice(ch->csmax_willpower, 7);
+        dam = godice(ch->csmax_willpower, 6);
     else
         dam = ch->level / 25;
 

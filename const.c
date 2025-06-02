7d2e3304e03c74b244c9ee22830723deb62d3dc1
Nicholas <njw352@gmail.com>
2025-06-02 14:25:14 -0400

    eyesoftheasp: added to prior commit, enabled (#505)

--- const.c
+++ const.c
@@ -2663,7 +2663,7 @@ Total (3) */
     &gsn_gift_distractions,          SLOT( 0),        0,     12, "",             "The yipping and howling in your ear subsides.",        "" },
 
     { "eye of the asp", { 102, 102,  102, 102, 102, 102, 102, 102, 102, 102, 102, 102, 102 },
-    { 1, 1, 1,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, spell_gift_eyeoftheasp,  TAR_IGNORE, POS_STANDING,
+    { 1, 1, 1,  1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, spell_gift_eyeoftheasp,  TAR_CHAR_DEFENSIVE, POS_STANDING,
     &gsn_gift_eyeoftheasp,          SLOT( 0),        0,     12, "",             "!!Wearoffmsg!!",        "" },
 
     { "song of rage", { 102, 102,  102, 102, 102, 102, 102, 102, 102, 102, 102, 102, 102 },

b4131369c709f3fc409468848add396329f9200b
Nicholas <njw352@gmail.com>
2025-06-23 17:46:12 -0400

    gifttable: activate reynards lie (closes #525)

--- tables.c
+++ tables.c
@@ -1403,7 +1403,7 @@ const struct    gift_type   gift_table  [MAX_GIFTS_CODED]  =
     {   "snow running", 				1,     24, 0,     0,      RAGABASH,   0,        1,   0},
     {   "sense of prey", 				1,     24, 0,     0,      RAGABASH,   BLACK_FURY, 2,   0},
     {   "open moon bridge", 			1,     24, 0,     0,      RAGABASH,   0,        3,   0},
-    {   "reynards lie", 				0,     24, 0,     0,      RAGABASH,   0,        3,   0},
+    {   "reynards lie", 				1,     24, 0,     0,      RAGABASH,   0,        3,   0},
     {   "lepers curse", 				0,     24, 0,     0,      RAGABASH,   0,        4,   0},
     {   "lunas blessing", 				0,     24, 0,     0,      RAGABASH,   0,        4,   0},
     {   "thieving talons of the magpie",0,     24, 0,     0,      RAGABASH,   0,        5,   0},
